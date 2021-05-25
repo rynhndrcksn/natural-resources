b136be26756f73ae693750acefbfaf13266e71a3
Alisa Llavore <allavore@mail.greenriver.edu>
2021-05-24 23:34:46 -0400

    Added applicant and application tables

--- test-resources/ntrs.sql
+++ test-resources/ntrs.sql
@@ -7,3 +7,20 @@ CREATE TABLE account(
     sid INT UNSIGNED NOT NULL,
     role TINYINT(1) NOT NULL
 );
+
+CREATE TABLE applicant (
+    applicantID INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    studentID INT UNSIGNED NOT NULL,
+    accountID INT UNSIGNED NOT NULL,
+    FOREIGN KEY (accountID) REFERENCES account(accountID)
+);
+
+CREATE TABLE application (
+    applicationID INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    degreeAudit VARCHAR(50),
+    unofficialTranscript VARCHAR(50),
+    personalStatement VARCHAR(50),
+    stepFourOption VARCHAR(50),
+    applicantID INT UNSIGNED NOT NULL,
+    FOREIGN KEY (applicantID) REFERENCES applicant(applicantID)
+);
