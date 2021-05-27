cc704fa09ab34ec8af7d53063eaa4939ba89700e
Ryan Rivera <rrivera@mail.greenriver.edu>
2021-05-26 22:28:30 -0400

    Modified Contorllers/login.php added TODO comments and test echo's of session objects, modified Models/loginmodel.php correcting missing dollar signs on result variable in User object input parameters

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -11,22 +11,27 @@ class Login extends BaseController
         if($this->request->getMethod() === 'post'){
 
             //TODO: Create validation method
+            //Get email and pass from POST array
             $email = $this->request->getPost('email');
             $pass = $this->request->getPost('pass');
 
+            //Create login model to handle queries to database
             $model = new LoginModel();
 
+            //If email and pass match an entry in database
+            //TODO: isUser and getUser are redundant, need to rework or consolidate them
             if($model->isUser($email, $pass)){
 
-                echo "TEST!!!";
+                //Start a session
+                $session = session();
 
-                //Started the session
-//                $session = session();
+                //Get the user info and save to session
+                $session->set('user', $model->getUser($email, $pass));
 
-                //Add user to session
-//                $session->set('user', $model->getUser($email, $pass));
-
-//                var_dump($session);
+                //For testing purposes
+                echo "<p>".$session->get('user')->getFirst()."</p>";
+                echo "<p>".$session->get('user')->getLast()."</p>";
+                echo "<p>".$session->get('user')->getEmail()."</p>";
             }
         }
 
--- app/Models/LoginModel.php
+++ app/Models/LoginModel.php
@@ -9,6 +9,7 @@ class LoginModel extends Model
 {
     protected $table = 'account';
 
+    //Verify user exists
     public function isUser($email, $pass){
 
         //Connect to the DB
@@ -28,25 +29,27 @@ class LoginModel extends Model
         return isset($row);
     }
 
-    //Validate user information
+    //Verify password matches user
     public function getUser($email, $pass): User
     {
-            //Connect to the DB
-            $db = db_connect();
+        //Connect to the DB
+        $db = db_connect();
 
-            //SQL query format
-            $sql = "SELECT * FROM account WHERE email = :email: AND pass = :pass:";
+        //SQL query format
+        $sql = "SELECT * FROM account WHERE email = :email: AND pass = :pass:";
 
-            //Send query, store results
-            $results = $db->query($sql, [
-                'email'     => $email,
-                'pass' => $pass
-            ]);
+        //Send query, store results
+        $results = $db->query($sql, [
+            'email'     => $email,
+            'pass' => $pass
+        ]);
 
-            //If user exists, add data to new User object and return it
-            if(!empty($results)){
-                return new User(results['email'], results['first'], results['last'], results['sid'], results['role'],
-                    results['program']);
-            }
+        $results = $results->getResultArray()[0];
+
+        //If results array is not empty
+        if(!empty($results)){
+            return new User($results['email'], $results['first'], $results['last'], $results['sid'], $results['role'],
+                $results['program']);
+        }
     }
 }
