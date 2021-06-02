f87a3de7a82477656c1b0e0739dfa9a1d15de501
Dana Clemmer <dclemmer2@mail.greenriver.edu>
2021-06-01 23:43:25 -0400

    Forgot to include this in last push

---
+++ app/Views/templates/footer.php
@@ -0,0 +1,119 @@
+<!-- Collapsible Footer -->
+<footer class="footer text-white text-decoration-none">
+    <!--  Top Row  -->
+    <div class="top-container row row-cols-1 row-cols-md-3 row-cols-lg-3 justify-content-center text-center mx-auto  pt-3">
+        <!--  Left Column  -->
+        <div class="col">
+            <a class="taber text-uppercase" data-toggle="collapse" data-parent="#accordion"
+               href="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                Student Resources
+            </a>
+            <div id="collapseOne" class="panel-collapse collapse in pt-2" role="tabpanel">
+                <hr class="background-white">
+                <ul>
+                    <li>
+                        <a class="hyperlink-removed" href="https://www.greenriver.edu/" target="_blank">Green
+                            River
+                            College</a>
+                    </li>
+                    <li>
+                        <a class="hyperlink-removed" href="https://login.microsoftonline.com/49d1e695-839f-40ae-a8b3-
+                        0a2de9d671b5/oauth2/authorize?client_id=8383e4ac-4bcd-493b-b37e-66f76f292346&redirect_uri=https
+                        %3A%2F%2Fmy.greenriver.edu%2Fsignin-oidc&response_type=id_token&scope=openid%20profile&response
+                        _mode=form_post&nonce=637508004443671642.MzVhNWMzMjQtNDdjNC00YjU5LTljMmMtM2NmNDIxNzcyM2U5ZjJmNz
+                        Y2NjYtYzU2OC00ZDY3LThmYTctNmQzODBlM2RiYzZi&state=CfDJ8ABaWuxjIk9NvJ80TUWqy1GZdLw7VGbq_m5JtUfM2T
+                        QvALRB8BXqVvPdhtwfJWb47OQiHkXhp1Nf5iJSNgVo2ThD2ykGnaY6X_Oa6dFCJRcQaD-oZZr4490u0NgMruHJNtMBUEg2N
+                        HMBdpRo8dec0ODAEjoc8vQsm6HeZ4HZuNd9nt_DXpfEz3Ty-fcBH3zKXpVCfUDfavL66-YZE904pKwtVb4vcdTsHrQzhfIH
+                        C7Sy7W2dfRKEgl9XcWNWRJeObObtWP0ZmhGkr8dX1LgSkerxL1D7zYw0pNChO4JymN3Q&x-client-SKU=ID_NETSTANDAR
+                        D2_0&x-client-ver=5.6.0.0&sso_reload=true" target="_blank">My Green River</a>
+                    </li>
+                    <li>
+                        <a class="hyperlink-removed" href="https://egator.greenriver.edu/" target="_blank">Canvas</a>
+                    </li>
+                    <li>
+                        <a class="hyperlink-removed"
+                           href="https://www.greenriver.edu/students/academics/dates/" target="_blank">
+                            Academic Calendar</a>
+                    </li>
+                    <li>
+                        <a class="hyperlink-removed"
+                           href="https://www.greenriver.edu/students/academics/enrollment-services/degree-audit/"
+                           target="_blank">Degree Audit</a>
+                    </li>
+                    <li>
+                        <a class="hyperlink-removed" href="https://www.greenriver.edu/students/academics/enrollment-services/" target="_blank">Office of the Registrar</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="col">
+            <a class="taber text-uppercase" data-toggle="collapse" data-parent="#accordion"
+               href="#collapsetwo" aria-expanded="false" aria-controls="collapseOne">
+                More Info
+            </a>
+            <div id="collapsetwo" class="panel-collapse collapse in pt-2" role="tabpanel">
+                <hr class="background-white">
+                <ul class="content">
+                    <li><a class="hyperlink-removed"
+                           href="https://www.greenriver.edu/students/academics/degrees-programs/natural-resources/"
+                           target="_blank">More
+                            About Natural Resources Program</a></li>
+                    <li><a class="hyperlink-removed"
+                           href="https://www.greenriver.edu/students/academics/degrees-programs/bachelor-of-applied-science/bachelor%E2%80%99s-in-forest-resource-management/meet-the-team/"
+                           target="_blank">Meet
+                            the Team</a></li>
+                    <li><a class="hyperlink-removed"
+                           href="https://catalog.greenriver.edu/content.php?filter%5B27%5D=NATRS&filter%5B29%5D=&filter%5Bcourse_type%5D=-1&filter%5Bkeyword%5D=&filter%5B32%5D=1&filter%5Bcpage%5D=1&cur_cat_oid=4&expand=&navoid=163&search_database=Filter#acalog_template_course_filter"
+                           target="_blank">Course
+                            Descriptions</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-4 col-lg-3">
+            <a class="taber text-uppercase" data-toggle="collapse" data-parent="#accordion"
+               href="#collapsethree" aria-expanded="false" aria-controls="collapseOne">
+                Contact Us
+            </a>
+
+            <div id="collapsethree" class="panel-collapse collapse in pt-2" role="tabpanel">
+                <hr class="background-white">
+                <ul>
+                    <li>
+                        <a class="hyperlink-removed" href="mailto:MPriebe@greenriver.edu">Dr. Monica Paulson
+                            Priebe</a>
+                    </li>
+                    <li>Natural Resources Faculty
+                        253-833-9111, ext. 4509
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <!--  Bottom -->
+    <div class="text-center copyright-text">
+        <p>© Created by AR2D2</p>
+    </div>
+</footer>
+</div>
+
+</div>
+</div>
+</body>
+
+<!-- JavaScript -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<script src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+<script src="//code.jquery.com/jquery.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+        integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
+        crossorigin="anonymous"></script>
+<script src="../public/assets/scripts/application.js"></script>
+<script src="../public/assets/scripts/navbar_and_footer.js"></script>
+
+</html>
---
+++ app/Views/templates/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+          integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'>
+    <link rel="stylesheet" href="../public/assets/styles/styles.css">
+
+    <title>NATRS Application Portal</title>
+    <!--Favicon-->
+    <link rel="icon" href="../public/assets/images/green-river-logo.png">
+</head>
+<body>
+
+<!--  Page Content  -->
+<div class="container-fluid h-100">
+
+    <!--  Row  -->
+    <div class="row h-100 justify-content-center">
+
+        <!--  Navbar Col  -->
+        <nav id="nav-col" class="navbar navbar-expand-md navbar-dark navbar-expand-lg col-md-3 col-lg-2 col-xl-2 text-center">
+
+            <!--  Navbar Logo  -->
+            <a id="grLogo" class="navbar-brand pt-2" href="#">
+                <img src="../public/assets/images/green-river-logo.png" alt="Green River College Logo">
+            </a>
+
+            <!--  Navbar Hamburger Menu  -->
+            <button id="hamburger" class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarToggler"
+                    aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
