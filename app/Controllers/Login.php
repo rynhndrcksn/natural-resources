390538fecd8e18d526efe1b47fb7a8ee0db7ebb5
Ryan Rivera <rrivera@mail.greenriver.edu>
2021-06-07 12:00:34 -0400

    added email/password validation and sticky forms to login page, also changed login page html to pull the title from the data array in the login contoroller

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -3,44 +3,74 @@
 namespace App\Controllers;
 
 use App\Models\LoginModel;
+use CodeIgniter\Model;
 
 
 class Login extends BaseController
 {
     public function index()
     {
+        $data = [];
+        $data['title'] = 'Login';
+
+
+        helper(['form']);
+        $rules = [
+            'email' => [
+                'rules' => 'required|valid_email',
+                'errors' => [
+                    'required' => 'Email address required',
+                    'valid_email' => 'Please enter a valid email address'
+                ]
+            ],
+            'pass' => [
+                'rules' => 'required',
+                'errors' => [
+                    'required' => 'Please enter your password'
+                ]
+            ]
+        ];
+
+
         //If request method == POST
         if ($this->request->getMethod() === 'post') {
 
-            //TODO: Create validation method for email and password
-            //Get email and pass from POST array
-            $email = $this->request->getPost('email');
-            $pass = $this->request->getPost('pass');
+            //If email and pass are valid
+            if($this->validate($rules)){
 
-            //Instantiate LoginModel to handle login queries to database
-            $model = new LoginModel();
+                //Get email and pass from POST array
+                $email = $this->request->getPost('email');
+                $pass = $this->request->getPost('pass');
 
-            //If email and pass match an entry in database
-            if ($model->isUser($email, $pass)) {
+                //Instantiate LoginModel to handle login queries to database
+                $model = new LoginModel();
 
-                //Get all user information and store in the session
-                $model->getUser($email, $pass);
+                //If email and pass match an entry in database
+                if ($model->isUser($email, $pass)) {
 
-                $data = $this->session->get('user');
-                $role = dot_array_search('role', $data);
-                //if user is an admin
-                if ($role == 1) {
-                    //Redirect
-                    return redirect()->to(base_url('/admin'));
+                    //Get all user information and store in the session
+                    $model->getUser($email, $pass);
+
+                    $data = $this->session->get('user');
+                    $role = dot_array_search('role', $data);
+                    //if user is an admin
+                    if ($role == 1) {
+                        //Redirect
+                        return redirect()->to(base_url('/admin'));
+                    }
+                    //if user is a student
+                    else if ($role == 0) {
+                        //Redirect
+                        return redirect()->to(base_url('/profile'));
+                    }
                 }
-                //if user is a student
-                else if ($role == 0) {
-                    //Redirect
-                    return redirect()->to(base_url('/profile'));
-                }
+            }
+            //Email and pass not valid
+            else{
+                $data['validation'] = $this->validator;
             }
         }
 
-        return view('pages/login.php');
+        return view('pages/login.php', $data);
     }
 }
